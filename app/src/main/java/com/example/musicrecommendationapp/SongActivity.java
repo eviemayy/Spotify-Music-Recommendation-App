83c64bda0fab28896d612c63cabde57fe43f0b58
Jonesy22 <Jonesy22@users.noreply.github.com>
2021-03-15 16:01:15 -0700

    X

--- app/src/main/java/com/example/musicrecommendationapp/SongActivity.java
+++ app/src/main/java/com/example/musicrecommendationapp/SongActivity.java
@@ -1,9 +1,15 @@
 package com.example.musicrecommendationapp;
 
 import android.content.SharedPreferences;
+<<<<<<< HEAD
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+=======
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+>>>>>>> cade-recycler-branch-merge
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,6 +20,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -24,13 +32,24 @@ public class SongActivity extends AppCompatActivity implements SharedPreferences
     private SongViewModel songViewModel;
     private RecyclerView songsRV;
     private SharedPreferences sharedPreferences;
+<<<<<<< HEAD
+=======
+
+>>>>>>> cade-recycler-branch-merge
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
 
-        this.songAdapter = new SongAdapter(this.generateSimpleList());
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String mood = sharedPreferences.getString(
+                getString(R.string.pref_mood_key),
+                getString(R.string.pref_mood_default_value)
+        );
+        Log.d("MOOOOOOOOOOOOOOD", "." + mood + ".");
+
+        this.songAdapter = new SongAdapter(this.generateSimpleList(mood));
         this.songsRV = findViewById(R.id.simple_recyclerview);
         this.songsRV.setLayoutManager(new LinearLayoutManager(this));
         this.songsRV.setHasFixedSize(true);
@@ -40,11 +59,33 @@ public class SongActivity extends AppCompatActivity implements SharedPreferences
         this.sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
-    private List<SongViewModel> generateSimpleList() {
-        List<SongViewModel> simpleViewModelList = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
-            simpleViewModelList.add(new SongViewModel(String.format(Locale.US, "This is song %d", i)));
+    private List<SongViewModel> generateSimpleList(String mood) {
+        List<Object> newList = Collections.emptyList();
+        Log.d("MOOOPOOOOOD", "SHOULD BE HERE NEXT");
+        if (mood.trim().equals("sad")) {
+            newList = Arrays.asList(getResources().getStringArray(R.array.sad_songs));
+        }
+        else if (mood.trim().equals("workout")) {
+            newList = Arrays.asList(getResources().getStringArray(R.array.workout_songs));
+        }
+        else if (mood.trim().equals("happy")) {
+            newList = Arrays.asList(getResources().getStringArray(R.array.happy_songs));
+        }
+        else if (mood.trim().equals("study")) {
+            newList = Arrays.asList(getResources().getStringArray(R.array.study_songs));
+        }
+        else if (mood.trim().equals("party")) {
+            newList = Arrays.asList(getResources().getStringArray(R.array.party_songs));
+        }
+        else {
+            newList = Arrays.asList(getResources().getStringArray(R.array.happy_songs));
+        }
+        List<SongViewModel> simpleViewModelList = new ArrayList<>();
+        //String songName = "    " + getString(R.string.party_song_still_brazy_name);
+
+        for (int i = 0; i < newList.size(); i++) {
+            simpleViewModelList.add(new SongViewModel(String.format(Locale.US, (String) newList.get(i))));
         }
 
         return simpleViewModelList;
